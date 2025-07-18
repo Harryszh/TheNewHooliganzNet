71ae87ad9cc632480e24fb601bf1277ad2805da0
harryszh <harrys.zh12@gmail.com>
2025-07-18 19:27:55 +0200

    ff

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,17 +1,16 @@
 // src/router/index.js
 import { createRouter, createWebHistory } from 'vue-router'
 
+
 // AUTH Pages
-import StartScreen from '@/views/auth/StartScreen.vue'
 import VereinsWahl from '@/views/auth/VereinsWahl.vue'
 import LoginPage from '@/views/auth/LoginPage.vue'
 import RegisterPage from '@/views/auth/RegisterPage.vue' // Register page import
 import RegistriPag from '@/views/auth/RegistriPag.vue' // Register page import 
 
+
 // MAIN Pages
 import HomeView from '@/views/HomeView.vue'
-
-
 
 
 // GAME Pages
@@ -22,35 +21,44 @@ import StadionView from '@/views/game/StadionView.vue'
 import BarPageView from '@/views/game/BarPageView.vue'
 import WCView from '@/views/game/WCView.vue'
 
+// MINI Spiel
+import PongMiniGame from '@/components/MiniGames/PongMiniGame.vue'
 
 // TEST Components
  import TestPage from '@/components/Auth/Test.vue' 
  import TeamDropdown from '@/components/TeamDropdown.vue'
-const routes = [
+ import StartScreen from '@/views/auth/StartScreen.vue'
+
+
+ const routes = [
   // Auth RoutePaths
   { path: '/', name: 'StartScreen', component: StartScreen },
-  { path: '/vereinswahl', name: 'Vereinswahl', component: VereinsWahl },
+  
   { path: '/login', name: 'LoginPage', component: LoginPage },
   { path: '/register', name: 'RegisterPage', component: RegisterPage}, // Register page route
   { path: '/registri', name: 'RegistriPag', component: RegistriPag}, // Register page route
 
+  
   // Main Routes
   { path: '/home', name: 'Home', component: HomeView },
-  
   
 
   // Game Routes
   
-  { path: '/pc', name: 'Pc', component: PcView },
+{ path: '/pc', name: 'Pc', component: PcView },
   { path: '/ligaplan', name: 'Ligaplan', component: LigaplanPageVue },
   { path: '/bar', name: 'Bar', component: BarPageView },
   { path: '/fridge', name: 'Fridge', component: FridgePageView },
   { path: '/stadion', name: 'Stadion', component: StadionView },
   { path: '/wc', name: 'WC', component: WCView },
 
+  // Mini Spiele
+  { path: '/pong', name: 'Pong', component: PongMiniGame },
+  
   // Test Route
   { path: '/test', name: 'Test', component: TestPage},
   { path: '/team-dropdown', name: 'TeamDropdown', component: TeamDropdown},
+  { path: '/vereinswahl', name: 'Vereinswahl', component: VereinsWahl },
   
 ]
    
